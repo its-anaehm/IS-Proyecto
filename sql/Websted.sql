b9e219f33e0986b78b870941f2c4096f8d6e9fc5
Luis <mluise2406@gmail.com>
2021-06-25 15:25:48 -0600

    DDS updated

--- sql/Websted.sql
+++ sql/Websted.sql
@@ -7,9 +7,9 @@ CREATE TABLE User(
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(15) NOT NULL,
-    var_email VARCHAR(50) UNIQUE NOT NULL,
+    txt_email VARCHAR(50) UNIQUE NOT NULL,
     telephone VARCHAR(20) NOT NULL,
-    txt_password VARCHAR(50) NOT NULL
+    txt_password VARCHAR(150) NOT NULL
 );
 
 
