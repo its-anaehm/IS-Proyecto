4cee0e9c15cd3e9b08bb0f8a149b3981edb03d65
Luis <mluise2406@gmail.com>
2021-06-25 10:03:52 -0600

    mysql and bcrypt modules added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from 'cors';
+import dotenv from "dotenv";
 
 import routes from './routes/index.routes';
 
+dotenv.config();
 const app = express();
 
-app.set('port', 4000 )
+app.set('port', process.env.PORT || 4000 )
 app.use(express.json());
 app.use(cors());
 
