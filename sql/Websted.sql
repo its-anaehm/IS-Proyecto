ddee5f0b0a82c11e13e21d6a4215e91897677e5b
Luis <mluise2406@gmail.com>
2021-06-25 12:43:07 -0600

    sql files added

---
+++ sql/Websted.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS Websted ;
+CREATE DATABASE Websted CHARACTER SET utf8;
+
+USE Websted;
+
+CREATE TABLE User(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15) NOT NULL,
+    var_email VARCHAR(50) UNIQUE NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    txt_password VARCHAR(50) NOT NULL
+);
+
+
