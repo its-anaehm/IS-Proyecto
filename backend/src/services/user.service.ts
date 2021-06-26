1beacc3af4f7728d3422e3290861d2caa873edf5
Luis <mluise2406@gmail.com>
2021-06-26 09:58:29 -0600

    Refactoring

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -9,7 +9,13 @@ export class UserService{
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(user.password, salt)
         try{
-           await db.query("INSERT INTO User(first_name, last_name, txt_email, telephone,txt_password) VALUES (?, ?, ?, ?, ?)", [user.firstName, user.lastName, user.email, user.phone, user.password])
+           await db.query("INSERT INTO User(first_name, last_name, txt_email, telephone,txt_password) VALUES (?, ?, ?, ?, ?)", 
+                            [user.firstName, 
+                            user.lastName, 
+                            user.email, 
+                            user.phone, 
+                            user.password]
+                        )
         }catch(err){
             throw err;
         }
