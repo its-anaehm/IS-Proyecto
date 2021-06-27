9351ec242b5c1e895ac4ddb4a118bdc78794a71d
Luis <mluise2406@gmail.com>
2021-06-27 11:21:04 -0600

    module morgan added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from "dotenv";
-import './database';
+import morgan from "morgan";
 
+import './database';
 import routes from './routes/index.routes';
 
 dotenv.config();
 const app = express();
 
-
-
 app.set('port', process.env.PORT || 4000 )
-app.use(express.json());
+
+app.use(morgan('dev'))
 app.use(cors());
+app.use(express.json());
 
 app.use('/',routes);
 
