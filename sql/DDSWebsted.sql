56ee4e6b3976807b566aa2f71545401d5aeb1147
Luis <mluise2406@gmail.com>
2021-07-06 10:03:27 -0600

    DDS script modfied

--- sql/DDSWebsted.sql
+++ sql/DDSWebsted.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS Websted ;
-CREATE DATABASE Websted CHARACTER SET utf8;
+CREATE DATABASE Websted;
 
 USE Websted;
 
@@ -9,8 +9,7 @@ CREATE TABLE Usuario(
     Apellido VARCHAR(15) NOT NULL,
     Email VARCHAR(50) UNIQUE NOT NULL,
     Telefono VARCHAR(20) NOT NULL,
-    Rol ENUM('Administrador', 'Usuario') DEFAULT 'Usuario',
-    Contrasena VARCHAR(150) NOT NULL
+    Contraseña VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Departamento(
@@ -31,7 +30,8 @@ CREATE TABLE Municipio(
 
 CREATE TABLE Categoria(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(40) NOT NULL
+    Nombre VARCHAR(40) NOT NULL,
+    Num_Visita  INT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE Producto(
@@ -40,7 +40,8 @@ CREATE TABLE Producto(
     Nombre VARCHAR(50) NOT NULL,
     Precio VARCHAR(15) NOT NULL,
     Descripcion VARCHAR(70) NOT NULL,
-    Fecha_Publicacion DATETIME DEFAULT NOW(),
+    Fecha_Publicación DATETIME DEFAULT NOW(),
+    Num_Visita  INT NOT NULL DEFAULT 0,
     Estado ENUM('Disponible','Vendido', 'Retirado'),
 
     FOREIGN KEY (fk_id_categoria)
@@ -72,16 +73,42 @@ CREATE TABLE Denuncia(
     fk_id_denunciador INT NOT NULL,
     fk_id_acusado INT NOT NULL,
     Fecha_denuncia DATETIME DEFAULT NOW(),
-    Tipo_Denuncia ENUM('Uno','Dos'),
-    Estado ENUM('No Revisado','Aprobado','Desestimado') DEFAULT 'No Revisado',
+    Tipo_Denuncia ENUM('',''),
+    Estado ENUM('Aprobado','Desestimado'),
 
     FOREIGN KEY (fk_id_denunciador)
         REFERENCES Usuario(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    
+
     FOREIGN KEY (fk_id_acusado)
         REFERENCES Usuario(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+CREATE TABLE Lista_Deseo(
+    fk_id_usuario INT NOT NULL,
+    fk_id_producto INT NOT NULL,
+
+    FOREIGN KEY (fk_id_usuario)
+        REFERENCES Usuario(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (fk_id_producto)
+        REFERENCES Producto(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE Imagen(
+    fk_id_producto INT NOT NULL,
+    Nombre VARCHAR(50),
+
+    FOREIGN KEY (fk_id_producto)
+        REFERENCES Producto(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
