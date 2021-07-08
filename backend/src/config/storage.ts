d2485b9d01c5aa1c1dbd76c278491bf32f546bd6
Luis <mluise2406@gmail.com>
2021-07-07 18:38:23 -0600

    multer config added

---
+++ backend/src/config/storage.ts
@@ -0,0 +1,20 @@
+import multer from "multer";
+import { v4 as uuid } from "uuid";
+import path from "path";
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/uploads/'));
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, uuid() + path.extname(file.originalname))
+    }
+});
+
+const fields = [
+    { name: "productImages", maxCount: 5},
+]
+
+export { storage, fields }
