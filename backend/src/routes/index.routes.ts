b18a46189475e9b6093f7f0c81d1d86f330fe9cc
itsdddaniel <daniel.arteaga@unah.hn>
2021-07-08 14:53:31 -0600

    New routes added

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import AccountRoutes from './user.routes';
 import ProductRoutes from './product.routes';
 import HomeRoutes from './home.routes';
+import FilterRoutes from './filter.routes';
+import CategoryRoutes from './category.routes'; 
 
 const router = Router()
 
 router.use('/users', AccountRoutes);
 router.use('/products', ProductRoutes);
 router.use('/home', HomeRoutes);
+router.use('/filters', FilterRoutes);
+router.use('/category', CategoryRoutes);
 
 export default router;
