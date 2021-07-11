bf332dd13b84e3caf67264d297c88c5afe3ea6b2
Luis <mluise2406@gmail.com>
2021-07-11 11:04:14 -0600

    get popular products endpoint added

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -4,7 +4,11 @@ import { verifyToken } from "../middlewares/auth.middleware";
 
 const routes = Router()
 
-/** Registrar un nuevo usuario */
+/** Obtener la lista de productos */
+routes.get('/', ProductController.getAllProducts)
+/** Obtener la lista de productos populares, limite 50 productos */
+routes.get('/popular', ProductController.getPopularProducts)
+/** Registrar un nuevo producto */
 routes.post('/', ProductController.addProduct)
 
 export default routes;
