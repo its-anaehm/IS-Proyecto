a851919e0328a7e197156fa8730a27d31efe6bbd
itsdddaniel <daniel.arteaga@unah.hn>
2021-07-13 15:09:07 -0600

    Correciones

--- backend/src/services/home.service.ts
+++ backend/src/services/home.service.ts
@@ -8,13 +8,6 @@ export class HomeService
         const [row] = await db.query('SELECT Usuario.Nombre AS "Usuario", Producto.Nombre AS "Nombre de Producto" FROM Lista_Deseo JOIN Usuario ON Lista_Deseo.fk_id_usuario = Usuario.id JOIN Producto ON Lista_Deseo.fk_id_producto = Producto.id WHERE Lista_Deseo.fk_id_usuario AND Lista_Deseo.fk_id_producto = ?', [id]);
         let jsonWishlist = JSON.parse(JSON.stringify(row));
         console.log(typeof(jsonWishlist));
-        if(jsonWishlist.length > 0)
-        {
-            return jsonWishlist;  
-        }
-        else
-        {
-            return "Lista de Deseo vacia.";
-        }
+        return jsonWishlist;
     }
 }
