91e88a5a3d858de66851159bc7dbbdf2b62b5196
itsdddaniel <daniel.arteaga@unah.hn>
2021-07-13 18:56:01 -0600

    Latest

--- backend/src/services/home.service.ts
+++ backend/src/services/home.service.ts
@@ -1,22 +1,14 @@
 import { db } from "../config/database";
-import User from "../models/User";
+import Product from "../models/Product";
 import ProductService from "./product.service";
 
 export class HomeService
 {
     public static wishlistList = async (id: Number) =>
     {
-        const [row] = await db.query('SELECT Usuario.Nombre AS "Usuario", Producto.Nombre AS "Nombre de Producto" FROM Lista_Deseo JOIN Usuario ON Lista_Deseo.fk_id_usuario = Usuario.id JOIN Producto ON Lista_Deseo.fk_id_producto = Producto.id WHERE Lista_Deseo.fk_id_usuario AND Lista_Deseo.fk_id_producto = ?', [id]);
-        let jsonWishlist = JSON.parse(JSON.stringify(row));
-        console.log(typeof(jsonWishlist));
-        if(jsonWishlist.length > 0)
-        {
-            jsonWishlist = await ProductService.getProductsImages(jsonWishlist)
-            return jsonWishlist;  
-        }
-        else
-        {
-            return "Lista de Deseo vacia.";
-        }
+        const [row] = await db.query('SELECT Producto.Nombre AS "name", Producto.Precio AS "price", Producto.Descripcion AS "description", Lista_Deseo.fk_id_producto AS "id" FROM Lista_Deseo JOIN Usuario ON Lista_Deseo.fk_id_usuario = Usuario.id JOIN Producto ON Lista_Deseo.fk_id_producto = Producto.id WHERE Lista_Deseo.fk_id_usuario = ?', [id]);
+        let jsonWishlist: Array<Product> = JSON.parse(JSON.stringify(row));
+        jsonWishlist = await ProductService.getProductsImages(jsonWishlist);
+        return jsonWishlist;
     }
 }
