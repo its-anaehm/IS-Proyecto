9ada9b0c601267057bd29d5641439d50c8a29fb1
itsdddaniel <daniel.arteaga@unah.hn>
2021-07-23 19:02:43 -0600

    Modified Wishlist Query and Published Products

--- backend/src/services/home.service.ts
+++ backend/src/services/home.service.ts
@@ -6,7 +6,7 @@ export class HomeService
 {
     public static wishlistList = async (id: Number) =>
     {
-        const [row] = await db.query('SELECT Producto.Nombre AS "name", Producto.Precio AS "price", Producto.Descripcion AS "description", Lista_Deseo.fk_id_producto AS "id" FROM Lista_Deseo JOIN Usuario ON Lista_Deseo.fk_id_usuario = Usuario.id JOIN Producto ON Lista_Deseo.fk_id_producto = Producto.id WHERE Lista_Deseo.fk_id_usuario = ?', [id]);
+        const [row] = await db.query('SELECT Lista_Deseo.fk_id_producto AS "id", Producto.Nombre AS "name", Producto.Precio AS "price", Imagen.Nombre AS "Imagen" FROM Lista_Deseo JOIN Usuario ON Lista_Deseo.fk_id_usuario = Usuario.id JOIN Producto ON Lista_Deseo.fk_id_producto = Producto.id JOIN Imagen on Imagen.fk_id_producto = Producto.id WHERE Lista_Deseo.fk_id_usuario = ?', [id]);
         let jsonWishlist: Array<Product> = JSON.parse(JSON.stringify(row));
         jsonWishlist = await ProductService.getProductsImages(jsonWishlist);
         return jsonWishlist;
