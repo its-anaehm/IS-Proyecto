bbdae96761360be4376bb1a10ac95c18c0a33b63
Daniel Arteaga <60668752+itsdddaniel@users.noreply.github.com>
2021-07-28 12:19:24 -0600

    Correciones

--- backend/src/services/comment.service.ts
+++ backend/src/services/comment.service.ts
@@ -4,7 +4,7 @@ export class CommentService
 {
     public static getComments = async(id: string) =>
     {
-        const [row] = await db.query(`SELECT CONCAT(Usuario.Nombre,' ',Usuario.Apellido) AS 'usuario', Producto.id, Producto.Nombre AS 'nombreProducto', Comentario.Comentario as 'contenido', DATE_FORMAT(Comentario.Fecha_Publicacion, '%y-%m-%d') AS 'fecha' FROM Comentario JOIN Producto ON Comentario.fk_id_producto = Producto.id JOIN Usuario ON Comentario.fk_id_usuario = Usuario.id WHERE Comentario.fk_id_producto = ?`, [id]);
+        const [row] = await db.query(`SELECT CONCAT(Usuario.Nombre,' ',Usuario.Apellido) AS 'usuario', Comentario.Comentario as 'contenido', DATE_FORMAT(Comentario.Fecha_Publicacion, '%y-%m-%d') AS 'fecha' FROM Comentario JOIN Producto ON Comentario.fk_id_producto = Producto.id JOIN Usuario ON Comentario.fk_id_usuario = Usuario.id WHERE Comentario.fk_id_producto = ? ORDER BY Comentario.Comentario DESC`, [id]);
         let jsonCategory = JSON.parse(JSON.stringify(row));
         return jsonCategory;
     }
