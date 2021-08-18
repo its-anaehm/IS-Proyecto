ce58972c5267021da88d46598d98124e8366b7a8
its-anaehm <aeherm0999@outlook.com>
2021-08-18 12:14:51 -0600

    Productos bonitos

--- frontend/src/componentes/ProductGrid.tsx
+++ frontend/src/componentes/ProductGrid.tsx
@@ -9,6 +9,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import { FormatAlignJustify } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -43,7 +44,9 @@ const useStyles = makeStyles((theme) => ({
   },
   root: {
     maxWidth: 345,
-    backgroundColor: 'lightgray'
+    backgroundColor: 'lightgray',
+    textDecoration: 'none',
+    position: 'relative'
   }
 }));
 
