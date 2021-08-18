7cc375061d4785e11bf62a8668f78f357ef3cbb9
Daniel Arteaga <60668752+itsdddaniel@users.noreply.github.com>
2021-08-18 13:21:05 -0600

    Added query.

--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -72,9 +72,12 @@ export default class ProductService{
     }
 
     public static getAllProductsNoPage = async () => {
-        const [row] = await db.query(`SELECT Producto.id, Producto.Nombre AS 'name', Departamento.Nombre AS 'departmento', Producto.Precio AS 'price' FROM Producto JOIN Departamento ON Producto.fk_id_departamento = Departamento.id`)
+        const [row] = await db.query(`SELECT id, fk_id_categoria AS category, fk_id_departamento AS department, fk_id_municipio AS municipy, Nombre AS name, Precio AS price, Descripcion AS details FROM Producto`)
+        // SELECT Producto.id, Producto.Nombre AS 'name', Departamento.Nombre AS 'departmento', Producto.Precio AS 'price' FROM Producto JOIN Departamento ON Producto.fk_id_departamento = Departamento.id
         
-        let productList = JSON.parse(JSON.stringify(row))
+        let productList : Array<Product> = (JSON.parse(JSON.stringify(row)))
+        productList = await ProductService.getProductsImages(productList)
+        
         return productList
     }
     
