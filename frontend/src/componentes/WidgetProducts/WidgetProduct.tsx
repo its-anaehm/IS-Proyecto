712e0a10f7883fbd2668049ef226400ccd98e234
its-anaehm <aeherm0999@outlook.com>
2021-08-18 18:32:46 -0600

    Modificaciones para el admin 0.1.2

--- frontend/src/componentes/WidgetProducts/WidgetProduct.tsx
+++ frontend/src/componentes/WidgetProducts/WidgetProduct.tsx
@@ -1,24 +1,95 @@
 import "./WidgetProduct.css";
 import { Link } from 'react-router-dom';
 import { Store, Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import ProductAdminObj from "../../interfaces/ProductAdminObj";
+import { Redirect } from "react-router-dom";
+import placeholder from "../../img/item-placeholder.png";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+let templateProducts: ProductAdminObj[] = [{
+    id: 0,
+    owner: "Juan Orlando",
+    department: "Honduras de alla",
+    municipy: "Tela",
+    name: "Baleada",
+    price: "13",
+    details: "Baleada con queso y horchata",
+    images: [`${placeholder}`],
+    date: "ayer",
+    Disponibilidad: "Disponible"
+}];
 
 function WidgetProduct(){
+
+    const [products, setProducts] = useState<ProductAdminObj[] | []>(templateProducts);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    function getProducts(){
+        fetch('http://localhost:4000/products/getAllProductsLimit',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+        }
+        ).then( response => {
+            if(response.status < 400){
+                response.json().then(jsonResponse => {
+                    console.log(jsonResponse)
+                    setProducts(jsonResponse);
+                })
+            }
+        }).catch(e=>{
+            console.log(e);
+        });
+    }
+
     return (
         <div className="WidgetProduct">
             <Link to="/catalogue" className="WidgetProductTitle">Productos</Link>
             <ul className="WidgetProductList">
                 <li className="WidgetProductListItem">
-                    <Store className="WidgetProducttIcon"/>
                     <div className="WidgetProductInfo">
-                        <span className="WidgetProductName">Producto 1</span>
-                        <span className="WidgetProductUser">Nombre del Dueño</span>
+                    <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell >Producto</TableCell>
+                                        <TableCell align="right">Precio</TableCell>
+                                        <TableCell align="right"></TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {products.map((data) => (
+                                <TableRow key={data.id}>
+                                    <TableCell component="th" scope="row" width="200">
+                                        {data.name}
+                                    </TableCell>
+                                    <TableCell align="right" width="200">{data.price}</TableCell>
+                                    <TableCell align="right" width="100">
+                                        <button className="buttom">
+                                            <Visibility className="widgetVSIcon"/>
+                                            <Link to="/other-user-profile/1" className="MostrarC">
+                                                Mostrar
+                                            </Link>
+                                        </button>                                        
+                                    </TableCell>
+                                </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
                     </div>
-                    <button className="buttom">
-                        <Visibility className="widgetVSIcon"/>
-                        <Link to="/other-user-profile/1" className="MostrarC">
-                            Mostrar
-                        </Link>
-                    </button>
                 </li>
             </ul>
         </div>
--- frontend/src/componentes/WidgetUsers/WidgetUsers.tsx
+++ frontend/src/componentes/WidgetUsers/WidgetUsers.tsx
@@ -1,24 +1,93 @@
 import { Person, Visibility } from "@material-ui/icons";
 import "./WidgetUsers.css";
 import { Link } from 'react-router-dom';
+import ComplaintObj from "../../interfaces/ComplaintObj";
+import { useState, useEffect } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 function WidgetUsers(){
+    
+    let templateProducts: ComplaintObj[] = [{
+        id: 0,
+        fk_id_denunciador: 0,
+        fk_id_acusado: 0,
+        NombreDenunciador: "string",
+        NombreAcusado: "string",
+        date: "string",
+        Tipo_Denuncia: 0,
+        Estado: "string"
+    }];
+    
+        const [complaint, setComplaint] = useState<ComplaintObj[] | []>(templateProducts);
+    
+        useEffect(() => {
+            getComplaint();
+        }, []);
+    
+        function getComplaint(){
+            fetch('http://localhost:4000/complaints/listcomplaintLimit',{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `${localStorage.getItem("USR_TKN")}`
+                }
+            }
+            ).then( response => {
+                if(response.status < 400){
+                    response.json().then(jsonResponse => {
+                        setComplaint(jsonResponse.message);
+                    })
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
+        }
+
     return (
+        
         <div className="WidgetUsers">
             <Link to="/complaint" className="widgetUserTitle">Denuncias de Usuarios</Link>
             <ul className="widgetUserList">
                 <li className="widgetUserListItem">
-                    <Person className="widgeUsertIcon"/>
                     <div className="widgetUserInfo">
-                        <span className="widgetUserName">Omen Truco</span>
-                        <span className="widgetUserDenuncia">Motivo de Denuncia</span>
+                        <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell >Acusado</TableCell>
+                                        <TableCell align="right" >Fecha</TableCell>
+                                        <TableCell align="right" ></TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                {complaint.map((data) => (
+                                <TableRow key={data.id}>
+                                    <TableCell component="th" scope="row" width="200">
+                                        {data.NombreAcusado}
+                                    </TableCell>
+                                    <TableCell align="right" width="200">{data.date}</TableCell>
+                                    <TableCell align="right" width="100">
+                                        <button className="buttom">
+                                            <Visibility className="widgetVSIcon"/>
+                                            <Link to="/other-user-profile/1" className="MostrarC">
+                                                Mostrar
+                                            </Link>
+                                        </button>                                        
+                                    </TableCell>
+                                </TableRow>
+                                ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
                     </div>
-                    <button className="buttom">
-                        <Visibility className="widgetVSIcon"/>
-                        <Link to="/other-user-profile/1" className="MostrarC">
-                            Mostrar
-                        </Link>
-                    </button>
                 </li>
             </ul>
         </div>
